9cd1345cb2ce6801294fe9276c1422bc554bb6b1
Cyiza2001 <82471629+Cyiza2001@users.noreply.github.com>
2024-03-14 17:42:55 +0200

    another commit

--- demo/demo/src/main/java/com/example/demo/student/studentController.java
+++ demo/demo/src/main/java/com/example/demo/student/studentController.java
@@ -1,20 +1,19 @@
-package com.example.demo;
-
+package com.example.demo.student;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-import com.example.demo.student.Student;
 import java.time.LocalDate;
 
 @RestController
-@RequestMapping(path ="/api/v1")
+@RequestMapping(path ="api/v1/student")
 public class studentController {
 
-    @GetMapping("/student")
+    @GetMapping("")
     public List<Student> getStudents() {
         return  List.of(
             new Student(1L,"Xander","imbwa@gmail.com", LocalDate.of(2001,9,15),23));
     }
 }
+
