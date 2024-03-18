0a20cb6178fc6bd2a5ef5d7134ee7a9a6442e3a7
Cyiza2001 <82471629+Cyiza2001@users.noreply.github.com>
2024-03-18 11:13:49 +0200

    another commit

--- demo/demo/src/main/java/com/example/demo/student/Student.java
+++ demo/demo/src/main/java/com/example/demo/student/Student.java
@@ -1,6 +1,9 @@
 package com.example.demo.student;
 
 import java.time.LocalDate;
+import java.time.Period;
+
+import org.springframework.data.annotation.Transient;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -27,24 +30,24 @@ public class Student {
     private String name;    
     private String email;   
     private LocalDate dob;
+    @Transient
     private Integer age;
 
     public Student() {
 
     }
     
-    public Student(Long id, String name, String email, LocalDate dob, Integer age){
+    public Student(Long id, String name, String email, LocalDate dob){
         this.id = id;
         this.name =name; 
         this.email = email; 
         this.dob = dob;
-        this.age = age; 
+
     }
-    public Student( String name, String email, LocalDate dob, Integer age){
+    public Student( String name, String email, LocalDate dob){
         this.name =name; 
         this.email = email; 
         this.dob = dob;
-        this.age = age; 
     }
     
     
@@ -77,9 +80,11 @@ return email;
 public LocalDate getDob(){ 
 return dob;
 }
+
 public Integer getAge() {
-return age;
+    return Period.between(this.dob, LocalDate.now()).getYears();
 }
+
 
     @Override
     public String toString() {
--- demo/demo/src/main/java/com/example/demo/student/StudentConfig.java
+++ demo/demo/src/main/java/com/example/demo/student/StudentConfig.java
@@ -2,22 +2,38 @@ package com.example.demo.student;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import
+import org.springframework.context.annotation.Configuration;
 
-@configuration 
+import java.time.LocalDate;
+// import java.time.Month;
+import java.util.List;
+
+
+@Configuration
 public class StudentConfig {
 
+    @SuppressWarnings("null")
     @Bean
  CommandLineRunner commandLineRunner (StudentRepository repository){
     return arg ->{
-        new Student(
-          id:1L,
-          name:"Xander",
-          email:"<EMAIL>",
-          dob:LocalDate.of(2001,9,15),
-          age:23
-
+        Student xander = new Student(
+          1L,
+          "Xander",
+          "kalisa@gmail.com",
+          LocalDate.of(2001,9, 15)
+   
         );
+        Student manyembua = new Student(
+          2L,
+          "manyembua",
+          "manyembua@gmail.com",
+          LocalDate.of(2008, 3, 13)
+       
+        );
+        // repository.saveAll(List.of(manyembua, xander));
+        repository.saveAll(List.of(xander, manyembua));
+        
     };
 }
 }
+
